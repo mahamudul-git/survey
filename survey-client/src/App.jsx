6ade563c2ca2b0aae0120a755b3249783c2a2574
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-07-20 18:10:19 +0600

    setup with tailwind

---
+++ survey-client/src/App.jsx
@@ -0,0 +1,17 @@
+
+import './App.css'
+
+function App() {
+  
+
+  return (
+    <div className='text-center'>
+
+      <h1 className='font-bold text-4xl'>Survey Client</h1>
+      <p>Welcome to the Survey Client application!</p>
+
+    </div>
+  )
+}
+
+export default App
