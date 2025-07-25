172ac4ced09cc204e2ec63ebecdb04888c6376ed
mahamudul-01 <mahamudul.hasan.mehedi.01@gmail.com>
2025-07-25 14:05:00 +0600

    fix: correct placeholder text in App component

--- survey-client/src/App.jsx
+++ survey-client/src/App.jsx
@@ -10,7 +10,7 @@ function App() {
       <h1 className='font-bold text-4xl'>Survey Client</h1>
       <p>Welcome to the Survey Client application!</p>
 
-      <p>Lorem ipsum dolor sit, amet consectetur adipisicing elit. A quia, sit adipisci repellendus quas magni minima sint corporis animi illo?</p>
+      <p>consectetur adipisicing elit. A quia, sit adipisci repellendus quas magni minima sint corporis animi illo?</p>
 
     </div>
   )
